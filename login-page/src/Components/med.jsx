2355c9e4c2ce1c399b37f11200edd6f759b120f3
Shehara <sheharadinithi.jayamanne@gmail.com>
2025-10-16 12:13:41 +0530

    Added navigation to chatbot

--- login-page/src/Components/med.jsx
+++ login-page/src/Components/med.jsx
@@ -1,13 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png"; // Your logo
 import ChatBot from "../Components/chatBot"; // Adjust path if needed
-import "../css/med.css";
+import "../css/med.css"; // Make sure navbar CSS is included
 
 export default function Med() {
   return (
     <div>
-      {/* Your existing page content here */}
-      
-      {}
-      <div style={{ marginTop: "20px" }}>
+      {/* NAVBAR */}
+      <header className="navbar">
+        <img src={logo} alt="Medicure Logo" className="logo" />
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About us</Link></li>
+          <li><Link to="/doctorView">Doctors</Link></li>
+          <li><Link to="/feedback">Feedback</Link></li>
+          <li><Link to="/location">Location</Link></li>
+          <li><Link className="active" to="/med">Medicine</Link></li>
+        </ul>
+      </header>
+
+      {/* ChatBot */}
+      <div style={{ marginTop: "120px" }}> {/* Push down below fixed navbar */}
         <ChatBot />
       </div>
     </div>
